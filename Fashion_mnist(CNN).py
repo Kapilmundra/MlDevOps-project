a4ae5e56b79d7daea2f30b25c1dd501c8fb68469
Kapil mundra <55842126+Kapilmundra@users.noreply.github.com>
2020-05-26 11:16:57 +0530

    Update Fashion_mnist(CNN).py

--- Fashion_mnist(CNN).py
+++ Fashion_mnist(CNN).py
@@ -105,7 +105,7 @@ print("Sending mail...")
 import smtplib
 s=smtplib.SMTP('smtp.gmail.com',587)
 s.starttls()
-s.login("mundrak21@gmail.com","qkmarorezknlbuuu")
+s.login("mundrak21@gmail.com","mjipeehczuzmsxyh")
 message1="success"
 s.sendmail("mundrak21@gmail.com","mundrak21@gmail.com",message1)
 s.quit()
